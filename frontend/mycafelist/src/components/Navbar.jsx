03b95a73295af995f32b6a539a2140cd2c8820d9
axelleagez <aagez@ensc.fr>
2025-03-04 16:13:06 +0100

    navbar

--- frontend/mycafelist/src/components/Navbar.jsx
+++ frontend/mycafelist/src/components/Navbar.jsx
@@ -1,34 +1,37 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
-import { Menu as MenuIcon, Home, AddCircle, List, Favorite, AccountCircle } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
+import { Home, AddCircle, List, Favorite, AccountCircle } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          MyCafeList
-        </Typography>
-        <IconButton color="inherit" component={Link} to="/">
-          <Home />
-        </IconButton>
-        <IconButton color="inherit" component={Link} to="/add">
-          <AddCircle />
-        </IconButton>
-        <IconButton color="inherit" component={Link} to="/list">
-          <List />
-        </IconButton>
-        <IconButton color="inherit" component={Link} to="/favorites">
-          <Favorite />
-        </IconButton>
-        <IconButton color="inherit" component={Link} to="/profile">
-          <AccountCircle />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <Paper
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 1000, // Assure que la navbar reste au-dessus du contenu
+      }}
+      elevation={3}
+    >
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+          navigate(newValue);
+        }}
+        showLabels={false} // Empêche l'affichage des labels sous les icônes
+      >
+        <BottomNavigationAction label="Accueil" icon={<Home />} value="/" />
+        <BottomNavigationAction label="Ajouter un café" icon={<AddCircle />} value="/add" />
+        <BottomNavigationAction label="Liste de mes cafés" icon={<List />} value="/list" />
+        <BottomNavigationAction label="Mes favoris" icon={<Favorite />} value="/favorites" />
+        <BottomNavigationAction label="Mon compte" icon={<AccountCircle />} value="/profile" />
+      </BottomNavigation>
+    </Paper>
   );
 }
